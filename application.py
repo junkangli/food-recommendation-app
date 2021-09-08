135c0ae90e9a43a00792b1c67babdbf3c16eeeb6
Li Junkang <junkang@singtel.com>
2021-09-09 00:33:13 +0800

    Fix problem running in elastic beanstalk environment

--- application.py
+++ application.py
@@ -2,11 +2,17 @@ from flask import Flask, render_template
 
 import query
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def index():
     data = []
     for area in query.list_all_areas():
         data.append({'area': area, 'count': query.count_meals_by_area(area)})
     return render_template('index.html', data=data)
+
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
