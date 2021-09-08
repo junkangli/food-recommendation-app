0203b28f949336c8f9cf7daefe1a5fbe6d9786c4
Li Junkang <junkang@singtel.com>
2021-09-08 20:55:30 +0800

    Display number of meals by area

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import requests
 
-api_url = "https://www.themealdb.com/api/json/v1/1/list.php?a=list"
-response = requests.get(api_url)
-result = response.json()
-areas = [area['strArea'] for area in result['meals']]
-print(areas)
+def list_all_areas():
+    api_url = "https://www.themealdb.com/api/json/v1/1/list.php?a=list"
+    response = requests.get(api_url)
+    result = response.json()
+    return [area['strArea'] for area in result['meals']]
+
+def count_meals_by_area(area):
+    api_url = f"https://www.themealdb.com/api/json/v1/1/filter.php?a={area}"
+    response = requests.get(api_url)
+    result = response.json()
+    return len(result['meals'])
+
+
+for area in list_all_areas():
+    print(f'{area} || {count_meals_by_area(area)}')
+
