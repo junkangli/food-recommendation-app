dca7937347d352506f46227b4ce52b573e3371e8
Li Junkang <junkang@singtel.com>
2021-09-10 23:20:03 +0800

    Add file upload function

--- application.py
+++ application.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 import query
 
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 application = Flask(__name__)
+application.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+application.config['UPLOAD_FOLDER'] = 'd:/temp/uploads'
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @application.route('/')
 def index():
@@ -35,6 +44,18 @@ def meal():
     id = request.args.get('id')
     meal = query.get_meal_by_id(id)
     return render_template('meal.html', meal=meal)
+
+@application.route('/search-by-photo', methods=['GET', 'POST'])
+def search_by_photo():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            print(f'photo posted {file.filename}')
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('search_by_photo', filename=filename))
+    filename = request.args.get('filename')
+    return render_template('search-by-photo.html', filename=filename)
 
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
